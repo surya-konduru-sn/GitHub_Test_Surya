89b0c264b2578e6336832f3bea8548c3c7bab550
rajkumar.tunuguntla <rajkumar.tunuguntla@servicenow.com>
2023-03-16 10:32:08 +0530

    2:randfile_2

--- createCOmmitsTags.sh
+++ createCOmmitsTags.sh
@@ -5,7 +5,7 @@ while [ $x -le 8000 ]
 do
  rm -rf random*
  string="touch randomfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
- echo $string
+ echo $string "HIIIIII"
  eval "$string"
  x=$[$x+1]
  git push
